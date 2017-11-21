c2c1b39e8351040d5b779f41b68c1260f665c296
Erwin Ried <erwinried@gmail.com>
2017-11-21 12:12:34 +0100

    Adding image crops

--- pybossa/parser/PybossaParser/PybossaParser/Program.cs
+++ pybossa/parser/PybossaParser/PybossaParser/Program.cs
@@ -11,9 +11,10 @@ namespace PybossaParser
     {
         static void Main(string[] args)
         {
-            const string inputFolder = "input", outputFolder = "output", inputTaskFile = "task.txt", inputTaskRunFile = "task_run.txt", outputFile = "areas.txt";
+            Environment.CurrentDirectory = @"D:\GitHub\price-tag-reader\pybossa\parser\Test";
+            const string inputFolder = "input", outputFolder = "output", outputFolderCrops = "cropped", inputTaskFile = "task.txt", inputTaskRunFile = "task_run.txt", outputFile = "areas.txt";
 
-            Regex regexTask = new Regex(@"(?<id>\d+).*?""url_raw"": ""(?<url>.*)"""), regexTaskRun = new Regex(@".*?\t.*?\t.*?\t(?<id>\d+)\t.*\[(?<areas>.*)\]"), 
+            Regex regexTask = new Regex(@"(?<id>\d+).*?""url_raw"": ""(?<url>.*)"""), regexTaskRun = new Regex(@"(?<idcrop>\d+)\t.*?\t.*?\t(?<id>\d+)\t.*\[(?<areas>.*)\]"), 
                 regexAreas = new Regex(@"""y"": (?<y>[\.\d]+).*?""x"": (?<x>[\.\d]+).*?""height"": (?<height>[\.\d]+).*?""width"": (?<width>[\.\d]+).*?""type"": ""(?<type>.*?)""");
 
             if (!Directory.Exists(outputFolder))
@@ -21,6 +22,7 @@ namespace PybossaParser
 
             // Download the images
             Console.WriteLine("Downloading images");
+        
             foreach (var line in File.ReadAllLines(Path.Combine(inputFolder,inputTaskFile)))
             {
                 var m = regexTask.Match(line);
@@ -55,7 +57,7 @@ namespace PybossaParser
                 if(m.Success)
                 {
                     var filename = $"{m.Groups["id"].Value}.jpg";
-
+                    var id = m.Groups["idcrop"].Value;
                     var mAreas = regexAreas.Matches(m.Groups["areas"].Value);
 
                     if (mAreas.Count == 0)
@@ -82,10 +84,27 @@ namespace PybossaParser
                                 var type = area.Groups["type"].Value;
 
                                 // Get correct proportions
-                                var rect = new RectangleF(float.Parse(area.Groups["x"].Value) * width, float.Parse(area.Groups["y"].Value) * height, float.Parse(area.Groups["w"].Value) * width, float.Parse(area.Groups["h"].Value) * height);
+                                var rect = new RectangleF(float.Parse(area.Groups["x"].Value) * width, float.Parse(area.Groups["y"].Value) * height, float.Parse(area.Groups["width"].Value) * width, float.Parse(area.Groups["height"].Value) * height);
 
                                 // Using ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
                                 output.Add($"['{filename}', '{width}', '{height}', '{type}', '{(int)rect.Left}', '{(int)rect.Top}', '{(int)rect.Right}', '{(int)rect.Bottom}']");
+
+                                // Cropping the picture
+                                var c = Path.Combine(outputFolder, outputFolderCrops);
+                                if (!Directory.Exists(c))
+                                    Directory.CreateDirectory(c);
+
+                                var outputImageCropsFolder = Path.Combine(c, Path.GetFileNameWithoutExtension(imageFile));
+                                if (!Directory.Exists(outputImageCropsFolder))
+                                    Directory.CreateDirectory(outputImageCropsFolder);
+
+                                var outputImageCropsSource = Path.Combine(outputImageCropsFolder, Path.GetFileName(imageFile));
+                                if (!File.Exists(outputImageCropsSource))
+                                    img.Save(outputImageCropsSource);
+
+                                // Save crop
+                                CropImage(img, rect).Save(Path.Combine(outputImageCropsFolder, $"crop_{id}.jpg"));
+                               
                             }
                         }
                     }
@@ -100,6 +119,13 @@ namespace PybossaParser
             Console.WriteLine($"Writing: {outputFile}");
             File.WriteAllLines(o, output.ToArray());
             Console.WriteLine("All done.");
+        }
+
+        // From: https://stackoverflow.com/questions/734930/how-to-crop-an-image-using-c
+        private static Image CropImage(Image img, RectangleF cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
         }
     }
 }
