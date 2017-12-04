1077ecfe32d177dca234413a4a4d9b4e9537f2b0
asadisaghar <asadisaghar@gmail.com>
2017-12-04 10:04:51 +0100

    lbp spar w numpos 200

--- haar_workspace/use_cascade.py
+++ haar_workspace/use_cascade.py
@@ -3,6 +3,8 @@ import cv2
 import sys
 import os
 import argparse
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cascade', '-c', type=str, required=True, help='supermarket name')
@@ -22,13 +24,17 @@ cascade = cv2.CascadeClassifier(cascade_path)
 img = cv2.imread(args['test'])
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-tags = cascade.detectMultiScale(gray, 1.3, 1)
+tags = cascade.detectMultiScale(gray, 1.3, 5)
+fig,ax = plt.subplots(1)
+ax.imshow(img)
 for (x,y,w,h) in tags:
-    cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+    rect = patches.Rectangle((x,y),w,h,linewidth=1,edgecolor='r',facecolor='none')
+    ax.add_patch(rect)
+#    cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
 
 #cv2.imshow('img', img)
-cv2.imwrite('detection.jpg', img)
+#cv2.imwrite('detection.jpg', img)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 
-
+plt.show()
