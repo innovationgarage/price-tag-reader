60dc4bbfeff49dfc2d8c97a7a37962e93a1428ad
asadisaghar <asadisaghar@gmail.com>
2017-12-06 16:22:58 +0100

    added args to the py script, made the sh script the same for LBP and HAAR

--- haar_workspace/train_cascade.sh
+++ haar_workspace/train_cascade.sh
@@ -4,14 +4,14 @@ path=$feature"_"$supermarket
 
 pos="positives/"
 neg="negatives/"
-numpos=500
-numneg=2000
-numstages=20
+numpos=1000
+numneg=1000
+numstages=5
 memuse=2000
 w=120
 h=80
 size=$w"x"$h"^"
-numThreads=4
+numThreads=7
 
 ## Pre-processing
 mkdir $pos
@@ -22,8 +22,10 @@ mkdir $path
 echo $path
 echo "grayscaling and resizing positive images"
 mogrify -path $pos -type Grayscale -resize $size ../haar_classifier/new_inputsets/$supermarket/*.jpg
-echo "grayscaling and resizing negative images"
-mogrify -path $neg -type Grayscale -resize $size ../haar_classifier/new_inputsets/negatives/*.jpg
+#mogrify -path $pos -type Grayscale ../haar_classifier/new_inputsets/$supermarket/*.jpg
+echo "grayscaling negative images"
+#mogrify -path $neg -type Grayscale -resize $size ../haar_classifier/new_inputsets/negatives/*.jpg
+mogrify -path $neg -type Grayscale ../haar_classifier/new_inputsets/negatives/*.jpg
 
 #make list
 cd $pos
@@ -34,15 +36,9 @@ ls -d "$PWD"/* > ../negatives.lst
 cd ..
 
 ##Create/augment training images
-perl createtrainsamples.pl positives.lst negatives.lst samples $numpos "opencv_createsamples -bgcolor 0 -bgthresh 80 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w "$w" -h "$h" -nosym"
+perl createtrainsamples.pl positives.lst negatives.lst samples $numpos "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w "$w" -h "$h""
 python mergevec.py -v samples/ -o positives.vec
 
-# ##Train the cascade
-# if [ $feature=='LBP' ];
-# then
-#     #LBP cascade
-opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -mode ALL -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature
-# else
-#     #Haar cascade
-#     opencv_traincascade -data $supermarket -vec positives.vec -bg negatives.lst -numStages $numstages -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -mode ALL -precalcValBufSize $memuse -precalcIdxBufSize $memuse
-# fi    
+##Train the cascade
+#opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature -mode ALL
+opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads  -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature
