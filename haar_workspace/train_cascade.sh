3dce39520ac812535c0d2e862bb17b6aef416023
root <root@s3.ymslanda>
2017-12-08 10:43:38 +0100

    new 7-stage haar cascade for rema

--- haar_workspace/train_cascade.sh
+++ haar_workspace/train_cascade.sh
@@ -6,12 +6,12 @@ pos="positives/"
 neg="negatives/"
 numpos=500
 numneg=2000
-numstages=20
+numstages=7
 memuse=2000
 w=120
 h=80
 size=$w"x"$h"^"
-numThreads=4
+numThreads=8
 
 ## Pre-processing
 mkdir $pos
@@ -34,15 +34,8 @@ ls -d "$PWD"/* > ../negatives.lst
 cd ..
 
 ##Create/augment training images
-perl createtrainsamples.pl positives.lst negatives.lst samples $numpos "opencv_createsamples -bgcolor 0 -bgthresh 80 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w "$w" -h "$h" -nosym"
+perl createtrainsamples.pl positives.lst negatives.lst samples $numpos "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w "$w" -h "$h""
 python mergevec.py -v samples/ -o positives.vec
 
-# ##Train the cascade
-# if [ $feature=='LBP' ];
-# then
-#     #LBP cascade
-opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -mode ALL -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature
-# else
-#     #Haar cascade
-#     opencv_traincascade -data $supermarket -vec positives.vec -bg negatives.lst -numStages $numstages -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -mode ALL -precalcValBufSize $memuse -precalcIdxBufSize $memuse
-# fi    
+##Train the cascade
+opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature #-mode ALL
