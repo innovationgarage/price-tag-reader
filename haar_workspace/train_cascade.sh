dc841ed67ea50eb49670273a08b861a28e3ceada
Saghar Asadi <asadisaghar@users.noreply.github.com>
2017-12-08 14:52:14 +0100

    Update train_cascade.sh

--- haar_workspace/train_cascade.sh
+++ haar_workspace/train_cascade.sh
@@ -4,24 +4,14 @@ path=$feature"_"$supermarket
 
 pos="positives/"
 neg="negatives/"
-<<<<<<< HEAD
-numpos=500
-numneg=2000
-numstages=7
-=======
 numpos=1000
 numneg=1000
 numstages=5
->>>>>>> 2ad83cc4d39efd787a5247d14fedd8d555042285
 memuse=2000
 w=120
 h=80
 size=$w"x"$h"^"
-<<<<<<< HEAD
 numThreads=8
-=======
-numThreads=7
->>>>>>> 2ad83cc4d39efd787a5247d14fedd8d555042285
 
 ## Pre-processing
 mkdir $pos
@@ -48,8 +38,5 @@ perl createtrainsamples.pl positives.lst negatives.lst samples $numpos "opencv_c
 python mergevec.py -v samples/ -o positives.vec
 
 ##Train the cascade
-<<<<<<< HEAD
-opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature #-mode ALL
-=======
-opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads  -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos $numpos -numNeg $numneg -w $w -h $h -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature -mode ALL
->>>>>>> 2ad83cc4d39efd787a5247d14fedd8d555042285
+opencv_traincascade -data $path -vec positives.vec -bg negatives.lst -numStages $numstages -numThreads $numThreads -minHitRate 0.999 -maxFalseAlarmRate 0.5 -bt GAB -maxDepth 1 -weightTrimRate 0.95 -maxWeakCount 100 -numPos $numpos -numNeg $numneg -w $w -h $h -precalcValBufSize $memuse -precalcIdxBufSize $memuse -featureType $feature -mode BASIC
+
